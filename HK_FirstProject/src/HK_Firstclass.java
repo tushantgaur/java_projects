4121b1cf8945413fa3587ddc9537cdbba240d4a5
test <test@ppbnonb2002521>
2020-12-10 17:10:06 +0530

    First java Project

---
+++ HK_FirstProject/src/HK_Firstclass.java
@@ -0,0 +1,12 @@
+import sun.nio.ch.sctp.SctpNet;
+
+import java.util.Scanner;
+
+public class HK_Firstclass {
+    public static void main(String args[]){
+        Scanner scan=new Scanner(System.in);
+        System.out.println("Hare Krishna");
+        int chanting_rounds=scan.nextInt();
+        System.out.println("How many rounds today :"+chanting_rounds);
+    }
+}
