3894acb64874bfe923449915e93c0365b1fd04a5
test <test@ppbnonb2002521>
2020-12-18 17:48:47 +0530

    Sorting Simplest

--- HK_ConstructorsManipulations/src/HK_Constructor.java
+++ HK_ConstructorsManipulations/src/HK_Constructor.java
@@ -1,52 +1,24 @@
-public class HK_Constructor {
-
-    int num1;
-    int num2;
-
-//    public HK_Constructor()
-//    {
-//        num1=10;
-//        num2=30;
-//    }
-//
-//    public HK_Constructor(int num1,int num2)
-//    {
-//        this.num1=num1;
-//        this.num2=num2;
-//    }
-
-    public  void display()
-    {
-        System.out.println("Outer Class");
-    }
-
-    public class Inner
-    {
-        public void show()
-        {
-            System.out.println("Inner Class");
-        }
-    }
-
-    public static class Static_InnerClass
-    {
-        public void showstaticinnerclass()
-        {
-            System.out.println("showstaticinnerclass Class");
-        }
-    }
-}
-
-class Tushant{
+class HK_Constructor{
 
     public static void main(String args[])
     {
-        HK_Constructor obj=new HK_Constructor();
-        obj.display();
-        HK_Constructor.Inner obj1= obj.new Inner();
-        obj1.show();
-        HK_Constructor.Static_InnerClass obj2=new HK_Constructor.Static_InnerClass();
-        obj2.showstaticinnerclass();
+        int a[]={30,20,10,50,5};
+
+        for (int i=0;i< a.length;i++)
+        {   for( int j=i+1;j<a.length;j++)
+            if(a[i]>a[j]) {
+                int temp = 0;
+                temp = a[j];
+                a[j] = a[i];
+                a[i] = temp;
+            }
+        }
+        for(int i=0;i< a.length;i++)
+        {
+            System.out.print(" "+a[i]);
+        }
+
+
 
     }
 }
